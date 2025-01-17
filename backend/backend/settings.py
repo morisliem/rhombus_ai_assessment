9cde62fa0480f5d8b94621bed8dcf96797e31bfc
rmoris <raja.moris@student.unimelb.edu.au>
2025-01-17 17:08:30 +0800

    add missing requirements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST', 'db'),  
-#         'PORT': os.environ.get('DB_PORT', '5432'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'db'),  
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
 
 
 # Password validation
--- backend/rhombus_ai/urls.py
+++ backend/rhombus_ai/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PatternMatchingAPI
 
 urlpatterns = [
-    path('pattern-matching/', PatternMatchingAPI.as_view(), name='pattern-matching'),
+    path('pattern-matching', PatternMatchingAPI.as_view(), name='pattern-matching'),
 ]
